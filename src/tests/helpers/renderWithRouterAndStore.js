5a584d2dcec6b13d86dd168ab8e3bbe8297c5fae
¨Venturin¨ <diegoventurinn@gmail.com>
2022-05-16 16:53:20 -0300

    test: add test cases for login page - requirement 04 done

---
+++ src/tests/helpers/renderWithRouterAndStore.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
+import { applyMiddleware, legacy_createStore as createStore } from 'redux';
+import { createMemoryHistory } from 'history';
+import thunk from 'redux-thunk';
+import { render } from '@testing-library/react';
+import reducer from '../../redux/reducers';
+
+
+// Funções getStore e renderWithRouterAndStore adaptadas a partir de uma outra solução desenvolvida pela Trybe
+const getStore = (initialState) => {
+  if (!initialState) return createStore(reducer, applyMiddleware(thunk));
+  return createStore(reducer, initialState, applyMiddleware(thunk));
+};
+
+const renderWithRouterAndStore = (component, routeConfigs = {}, initialState) => {
+  const route = routeConfigs.route || '/';
+  const store = getStore(initialState);
+  const history = routeConfigs.history
+        || createMemoryHistory({ initialEntries: [route] });
+
+  return {
+    ...render(
+      <Provider store={ store }>
+        <Router history={ history }>{component}</Router>
+      </Provider>,
+    ),
+    history,
+    store,
+  };
+};
+
+export default renderWithRouterAndStore;
---
+++ src/tests/login.test.js
@@ -0,0 +1,131 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouterAndStore from './helpers/renderWithRouterAndStore';
+import { waitFor } from '@testing-library/react';
+
+const EMAIL_INPUT_TEST_ID = 'input-gravatar-email';
+const NAME_INPUT_TEST_ID = 'input-player-name';
+const BUTTON_PLAY_TEST_ID = 'btn-play';
+const BUTTON_SETTINGS_TEST_ID = 'btn-settings';
+const SETTINGS_TITLE_TEST_ID = 'settings-title';
+
+const VALID_EMAIL = 'email@correto.com';
+const VALID_NAME = 'Diego Venturin';
+
+describe('Testa o componente <Login.js />.', () => {
+
+  test('A primeira rota deve ser para /', () => {
+    const { history } = renderWithRouterAndStore(<App />);
+    expect(history.location.pathname).toBe('/');
+  });
+
+  test('A página deve ter um campo imput para o usuário inserir o email', () => {
+    renderWithRouterAndStore(<App />);
+    const email = screen.getByTestId(EMAIL_INPUT_TEST_ID);
+    expect(email).toBeInTheDocument();
+  });
+
+  test('A página deve ter um campo imput para o usuário inserir o nome', () => {
+    renderWithRouterAndStore(<App />);
+    const name = screen.getByTestId(NAME_INPUT_TEST_ID);
+    expect(name).toBeInTheDocument();
+  });
+
+  test('A página deve ter um botão para o usuário iniciar o jogo', () => {
+    renderWithRouterAndStore(<App />);
+    const btnPlay = screen.getByTestId(BUTTON_PLAY_TEST_ID);
+    expect(btnPlay).toBeInTheDocument();
+  });
+
+  test('A página deve ter um botão para o usuário acessar as configurações', () => {
+    renderWithRouterAndStore(<App />);
+    const btnSettings = screen.getByTestId(BUTTON_SETTINGS_TEST_ID);
+    expect(btnSettings).toBeInTheDocument();
+  });
+
+  test('O botão só deve estar habilitado com email e senha preenchidos', () => {
+    renderWithRouterAndStore(<App />);
+    const btnPlay = screen.getByTestId(BUTTON_PLAY_TEST_ID);
+    expect(btnPlay).toBeDisabled();
+
+    const email = screen.getByTestId(EMAIL_INPUT_TEST_ID);
+    const name = screen.getByTestId(NAME_INPUT_TEST_ID);
+
+    userEvent.type(email, VALID_EMAIL);
+    expect(btnPlay).toBeDisabled();
+
+    userEvent.type(name, VALID_NAME);
+    expect(btnPlay).not.toBeDisabled();
+  });
+
+  test('As informações do jogador dever ser armazenadas no Estado Global', () => {
+    const { store } = renderWithRouterAndStore(<App />);
+    const btnPlay = screen.getByTestId(BUTTON_PLAY_TEST_ID);
+    const email = screen.getByTestId(EMAIL_INPUT_TEST_ID);
+    const name = screen.getByTestId(NAME_INPUT_TEST_ID);
+
+    userEvent.type(email, VALID_EMAIL);
+    userEvent.type(name, VALID_NAME);
+    userEvent.click(btnPlay);
+
+    expect(store.getState().player.name).toStrictEqual(VALID_NAME);
+    expect(store.getState().player.gravatarEmail).toStrictEqual(VALID_EMAIL);
+
+  });
+
+  test('Ao clicar no botão Play o usuário deve ser redirecionado para /play', () => {
+    const { history } = renderWithRouterAndStore(<App />);
+    const btnPlay = screen.getByTestId(BUTTON_PLAY_TEST_ID);
+    const email = screen.getByTestId(EMAIL_INPUT_TEST_ID);
+    const name = screen.getByTestId(NAME_INPUT_TEST_ID);
+
+    userEvent.type(email, VALID_EMAIL);
+    userEvent.type(name, VALID_NAME);
+    userEvent.click(btnPlay);
+
+    expect(history.location.pathname).toBe('/play');
+  });
+
+  test('Ao clicar no botão Settings o usuário deve ser redirecionado para /settings', () => {
+    const { history } = renderWithRouterAndStore(<App />);
+    const btnSettings = screen.getByTestId(BUTTON_SETTINGS_TEST_ID);
+  
+    userEvent.click(btnSettings);
+
+    expect(history.location.pathname).toBe('/settings');
+    const settingsTitle = screen.getByTestId(SETTINGS_TITLE_TEST_ID);
+    expect(settingsTitle).toBeInTheDocument();
+  });
+
+  test('Ao clicar no botão Play um token deve ser requisitado na API e armazenado no estado global e localStorage', async () => {
+    const { store } = renderWithRouterAndStore(<App />);
+    const btnPlay = screen.getByTestId(BUTTON_PLAY_TEST_ID);
+    const email = screen.getByTestId(EMAIL_INPUT_TEST_ID);
+    const name = screen.getByTestId(NAME_INPUT_TEST_ID);
+
+    const token = "f00cb469ce38726ee00a7c6836761b0a4fb808181a125dcde6d50a9f3c9127b6";
+    const responseAPI = {
+            "response_code":0,
+            "response_message":"Token Generated Successfully!",
+            token,
+         };
+
+    jest.spyOn(global, 'fetch');
+    global.fetch.mockResolvedValue({
+        json: jest.fn().mockResolvedValue(responseAPI),
+    });
+
+    userEvent.type(email, VALID_EMAIL);
+    userEvent.type(name, VALID_NAME);
+    userEvent.click(btnPlay);
+
+    expect(global.fetch).toBeCalledTimes(1);
+    await waitFor(() => expect(store.getState().token).toStrictEqual(token));
+
+    const tokenLocalStorage = localStorage.getItem('token');
+    expect(tokenLocalStorage).toStrictEqual(token);
+
+  });
+});
