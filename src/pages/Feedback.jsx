8bb309596347d57489ac0726447aee08664837db
¨Venturin¨ <diegoventurinn@gmail.com>
2022-05-17 09:28:04 -0300

    feat: requirement 14 done

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 class Feedback extends React.Component {
   render() {
+    const { assertions } = this.props;
+    const minAssertions = 3;
     return (
       <div>
         <Header />
-        <h1 data-testid="feedback-text">Feedback Page</h1>
+        {(assertions < minAssertions)
+          ? <p data-testid="feedback-text">Could be better...</p>
+          : <p data-testid="feedback-text">Well Done!</p>}
       </div>
     );
   }
 }
 
-export default Feedback;
+const mapStateToProps = (state) => ({
+  assertions: state.player.assertions,
+});
+
+export default connect(mapStateToProps)(Feedback);
+
+Feedback.propTypes = {
+  assertions: PropTypes.number.isRequired,
+};
