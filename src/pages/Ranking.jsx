6719b7179eeb87c52d4c3f16b413c2fd88e44bcf
¨Venturin¨ <diegoventurinn@gmail.com>
2022-05-17 10:15:14 -0300

    feat: requirement 19 done

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
 
 class Ranking extends React.Component {
-  render() {
-    return (
-      <h1 data-testid="ranking-title">Ranking Page</h1>
-    );
-  }
+    state = {
+      redirectToLogin: false,
+    }
+
+    render() {
+      const { redirectToLogin } = this.state;
+      if (redirectToLogin) {
+        return <Redirect to="/" />;
+      }
+      return (
+        <div>
+          <h1 data-testid="ranking-title">Ranking Page</h1>
+          <button
+            type="button"
+            data-testid="btn-go-home"
+            onClick={ () => { this.setState({ redirectToLogin: true }); } }
+          >
+            Play Again
+          </button>
+        </div>
+      );
+    }
 }
 
 export default Ranking;
