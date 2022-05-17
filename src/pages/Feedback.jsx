916d355c7c10ae8509d2317db4a51ba98ad75c69
¨Venturin¨ <diegoventurinn@gmail.com>
2022-05-17 09:35:16 -0300

    feat: requirement 15 done

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -5,7 +5,7 @@ import Header from '../components/Header';
 
 class Feedback extends React.Component {
   render() {
-    const { assertions } = this.props;
+    const { assertions, score } = this.props;
     const minAssertions = 3;
     return (
       <div>
@@ -13,6 +13,14 @@ class Feedback extends React.Component {
         {(assertions < minAssertions)
           ? <p data-testid="feedback-text">Could be better...</p>
           : <p data-testid="feedback-text">Well Done!</p>}
+        <p>Total Hits</p>
+        <p data-testid="feedback-total-question">
+          {assertions}
+        </p>
+        <p>Total Score:</p>
+        <p data-testid="feedback-total-score">
+          {score}
+        </p>
       </div>
     );
   }
@@ -20,10 +28,12 @@ class Feedback extends React.Component {
 
 const mapStateToProps = (state) => ({
   assertions: state.player.assertions,
+  score: state.player.score,
 });
 
 export default connect(mapStateToProps)(Feedback);
 
 Feedback.propTypes = {
   assertions: PropTypes.number.isRequired,
+  score: PropTypes.number.isRequired,
 };
