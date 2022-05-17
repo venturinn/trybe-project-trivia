31ed010edd880bacca01480e1e180093dc1521b7
¨Venturin¨ <diegoventurinn@gmail.com>
2022-05-17 09:19:04 -0300

    feat: requirement 13 done

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from '../components/Header';
 
 class Feedback extends React.Component {
   render() {
     return (
       <div>
+        <Header />
         <h1 data-testid="feedback-text">Feedback Page</h1>
       </div>
     );
